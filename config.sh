b23f1899c6115e7f55823ea738aec053865e77cc
xoviat <xoviat@users.noreply.github.com>
2017-12-04 19:06:18 -0600

    Fix config

--- config.sh
+++ config.sh
@@ -4,60 +4,15 @@
 # Enable Python fault handler on Pythons >= 3.3.
 PYTHONFAULTHANDLER=1
 
-# OpenBLAS version for systems that use it.
-OPENBLAS_VERSION=0.2.18
-
-source gfortran-install/gfortran_utils.sh
-
 function pre_build {
-    if [ -n "$IS_OSX" ]; then brew update; fi  # Update to get suite-sparse formula
+    if [ -n "$IS_OSX" ];
+        then brew update; # Update to get suite-sparse formula
+    else
+        build_openblas
+    fi
     # Install the build dependencies
     build_swig
     build_suitesparse
-}
-
-function build_wheel {
-    if [ -z "$IS_OSX" ]; then
-        build_libs $PLAT
-        build_pip_wheel $@
-    else
-        build_osx_wheel $@
-    fi
-}
-
-function build_libs {
-    if [ -n "$IS_OSX" ]; then return; fi  # No OpenBLAS for OSX
-    local plat=${1:-$PLAT}
-    local tar_path=$(abspath $(get_gf_lib "openblas-${OPENBLAS_VERSION}" "$plat"))
-    (cd / && tar zxf $tar_path)
-}
-
-function set_arch {
-    local arch=$1
-    export CC="clang $arch"
-    export CXX="clang++ $arch"
-    export CFLAGS="$arch"
-    export FFLAGS="$arch"
-    export FARCH="$arch"
-    export LDFLAGS="$arch"
-}
-
-function build_osx_wheel {
-    # Build dual arch wheel
-    # Standard gfortran won't build dual arch objects, so we have to build two
-    # wheels, one for 32-bit, one for 64, then fuse them.
-    local repo_dir=${1:-$REPO_DIR}
-    local py_ld_flags="-Wall -undefined dynamic_lookup -bundle"
-
-    install_gfortran
-    # 64-bit wheel
-    local arch="-m64"
-    set_arch $arch
-    build_libs x86_64
-    # Build wheel
-    export LDSHARED="$CC $py_ld_flags"
-    export LDFLAGS="$arch $py_ld_flags"
-    build_pip_wheel "$repo_dir"
 }
 
 function run_tests {
